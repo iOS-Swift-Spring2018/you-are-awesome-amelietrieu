685ccedacd39703dbfd349d570e296e24a7da9f6
Amelie Trieu <amelette@amelies-mbp.bc.edu>
2018-01-22 12:45:48 -0500

    Initial Commit

---
+++ Lesson 2.1 You are Awesome!/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lesson 2.1 You are Awesome!
+//
+//  Created by Amelie Trieu on 1/22/18.
+//  Copyright © 2018 Amelie Trieu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
